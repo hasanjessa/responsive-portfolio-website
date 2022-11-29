d91edaeb20aacb881ea50d03c6508ec904b6c5da
Hasan Jessa <hasan.jessa@talentpathway.com>
2022-11-29 17:55:43 -0600

    added contact form and swiper for projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,12 +31,53 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56
+        }
+    }
+});
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactProject = document.getElementById('contact-project'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // check if the field has a value and is not being submitted empty
+    if (contactName.value === '' || contactEmail.value === '', contactProject.value === '') {
+        // add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        // show message
+        contactMessage.textContent = 'Write all the input fields'
+    } else {
+
+        // serviceId, templateId, #form, publicKey
+        emailjs.sendForm('', '', '', '')
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
